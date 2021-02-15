243c09d656a7b4abdb4ac7bfae8271bca75a9ee1
Joel Malissa <jmaliska@gmail.com>
2021-02-15 01:53:10 -0500

    Updated browser extension to 1.9

--- README.txt
+++ README.txt
@@ -1,9 +1,9 @@
 This expands over 25,000 NASA acronyms.
 
+Search engine: https://nasaacronyms.com
+
 Chrome: https://chrome.google.com/webstore/detail/nasa-acronyms/anpbkdhjbebjjkgdbglbcfaenjldbinf
 
 Firefox: https://addons.mozilla.org/addon/nasa-acronyms
-
-Search engine: https://nasaacronyms.com
 
 Authors: Joel Malissa & Logan Stafman
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// Joel Malissa & Logan Stafman
+//Joel Malissa & Logan Stafman
 
-// maps from acronym to meaning
+//maps from acronym to meaning
 var myMap = {};
 
-$.getJSON('https://raw.githubusercontent.com/maliska/NASA-Acronyms/master/acronyms.json', function getAcronymsJson(data) {
+$.getJSON('https://raw.githubusercontent.com/nasa/NASA-Acronyms/master/lists/acronyms.json', function getAcronymsJson(data) {
     for(var i = 0; i < data.length; i++) {
         var abbrev = data[i]['abbreviation'].toLowerCase();
         if(!myMap[abbrev])
@@ -39,7 +39,8 @@ $(function renderTooltip() {
                     w = rect.width;
                 }
             }
-            var selText = selection.toString().trim().toLowerCase().replace(/\./g,'');
+            selection = selection.toString().trim();
+            var selText = selection.toLowerCase().replace(/\./g,'');
             var meanings;
             if(myMap[selText]) {
                 var meanings = new Array();
@@ -47,18 +48,39 @@ $(function renderTooltip() {
                     meanings.push(myMap[selText][i]['expansion']);
                 }
             }
-            $('#nasa_tooltip').remove();
-            if(selText.length > 1 && meanings) {
+
+            $('.qtip').remove(); // Does this line fix the top left corner duplicate qtip bug?
+            $('#nasa_tooltip').remove(); // removes the div created to anchor the first qtip
+            
+            // Create tooltip with number of results displayed
+            if (selText.length > 1 && meanings) {
+                // Sort meanings here
+                // meanings.sort();  // i.e. sort alphabetically
+                // console.log('alphabetical order: ' + meanings);
+                // meanings.sort(function(a, b){return a.length - b.length}); // i.e. shortest first
+                // console.log('shortest first: ' + meanings);
+                // meanings.sort(function(a, b){return b.length - a.length}); // i.e. longest first
+                // console.log('longest first: ' + meanings);
+                meanings.sort(function(a, b){ // sort by appearances on page
+                    var regA = new RegExp(a, 'gi');
+                    var regB = new RegExp(b, 'gi');
+                    return (document.body.innerText.match(regB) || []).length - (document.body.innerText.match(regA) || []).length;
+                })
+
+                if (meanings.length == 1) { // 1 result
+                  var out = meanings.length + ' acronym found for ' + selection + ':<br>';
+                } else { // >1 result
+                  var out = meanings.length + ' acronyms found for ' + selection + ':<br>';
+                }
+                for(var i = 0; i < meanings.length; i++) out += '\u2022  ' + meanings[i] + '<br>';
+                out = out.trim();
+
                 var div = document.createElement('div');
                 div.style.position = 'fixed';
                 div.style.left = x + 'px';
                 div.style.top = y + 'px';
                 div.style.height = h + 'px';
                 div.style.width = w + 'px';
-                var meaning = '';
-                for(var i = 0; i < meanings.length; i++) meaning += '\u2022  ' + meanings[i] + '<br>';
-                meaning = meaning.trim();
-                div.setAttribute('title', meaning);
                 div.setAttribute('id', 'nasa_tooltip');
                 document.body.appendChild(div);
    
@@ -67,7 +89,7 @@ $(function renderTooltip() {
                     hide: {
                         event: 'unfocus'
                     }, // hides qtip on mousedown
-                    content: meaning,
+                    content: out,
                     prerender: true,
                     show: {
                         ready: true
